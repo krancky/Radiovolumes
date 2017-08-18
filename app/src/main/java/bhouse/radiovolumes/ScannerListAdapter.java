c6b86bd62aed09280d12a86a4f39301e3fa5cb05
krancky <kranck@gmail.com>
2017-08-18 22:02:40 +0200

    2

--- app/src/main/java/bhouse/radiovolumes/ScannerListAdapter.java
+++ app/src/main/java/bhouse/radiovolumes/ScannerListAdapter.java
@@ -46,34 +46,32 @@ public class ScannerListAdapter extends ArrayAdapter<SliceItem>{
             holder = new ViewHolder();
             SliceItem item = getItem(position);
             holder.scanView = (ImageView)convertView.findViewById(R.id.view_scan);
-            holder.frameLayout = (FrameLayout) convertView.findViewById(R.id.zoomView);
+            holder.frameLayout = (FrameLayout) convertView.findViewById(R.id.zoomLayout);
+
+            int resIdScan = this.context.getResources().getIdentifier(item.getStorageLocation(), "drawable", context.getPackageName());
+            Picasso
+                    .with(context)
+                    .load(resIdScan)
+                    .error(R.drawable.borabora)
+                    .into(holder.scanView);
+
+
+            for (int i = 0; i < item.getVectorStorageLocation().size(); i++){
+                ImageView imageView = new ImageView(context);
+                String truc = item.getVectorStorageLocation().get(i);
+                //int resId = context.getResources().getIdentifier( item.getVectorStorageLocation().get(i), "drawable", context.getPackageName());
+                String resourceName = "cylindre__2___"+String.valueOf(position);
+                int resId = context.getResources().getIdentifier( resourceName, "drawable", context.getPackageName());
+                    imageView.setImageResource(resId);
+                    holder.frameLayout.addView(imageView);
+            }
+            convertView.setTag(holder);
 
         }
         else{
             holder = (ViewHolder)convertView.getTag();
-        }
 
-        SliceItem item = getItem(position);
-
-        int i;
-        for (i = 0; i < item.getVectorStorageLocation().size(); i++){
-            ImageView imageView = new ImageView(holder.frameLayout.getContext());
-            String truc = item.getVectorStorageLocation().get(i);
-            int resId = context.getResources().getIdentifier( item.getVectorStorageLocation().get(i), "drawable", context.getPackageName());
-            imageView.setImageResource(resId);
-            holder.frameLayout.addView(imageView);
-        }
-        convertView.setTag(holder);
-        //ImageView imageView = (ImageView)convertView.findViewById(R.id.slice_item_imageView);
-        //imageView.setImageResource(0);
-
-        int resId = this.context.getResources().getIdentifier(item.getStorageLocation(), "drawable", context.getPackageName());
-        Picasso
-                .with(context)
-                .load(resId)
-                .error(R.drawable.borabora)
-                .into(holder.scanView);
-
+            }
 
         return convertView;
 
@@ -83,8 +81,5 @@ public class ScannerListAdapter extends ArrayAdapter<SliceItem>{
     {
         FrameLayout frameLayout;
         ImageView scanView;
-        ImageView[] vectorView;
-        // va falloir en mettre un paquet en dynamique. Ou alors tous les mettre mais des fois null.
-
     }
 }
